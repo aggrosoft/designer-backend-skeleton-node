49c892b8ed0ce26df0c8578ff690fbaeb40044c1
alexander <coding@aggrosoft.de>
2021-01-20 11:53:09 +0100

    default pm2 config to config.json instead config.sample.json

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 const fs = require('fs')
 const path = require('path')
+const configFile = path.resolve(argv.config)
+
+if (!fs.existsSync(configFile)){
+  throw new Error('Config file ' + argv.config + ' does not exist, copy config.sample.json to ' + argv.config + ' before first boot!')
+}
 
 const app = require('designer-backend-node')
 app.serve(
