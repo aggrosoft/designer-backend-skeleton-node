0e45c2308359e46bbf8bc7790a0729b43b52531c
alexander <coding@aggrosoft.de>
2021-01-21 17:22:18 +0100

    add migrate script

---
+++ migrate.js
@@ -0,0 +1,13 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const fs = require('fs')
+const path = require('path')
+const configFile = path.resolve(argv.config)
+
+if (!fs.existsSync(configFile)){
+  throw new Error('Config file ' + argv.config + ' does not exist, copy config.sample.json to ' + argv.config + ' before first boot!')
+}
+
+const migrator = require('designer-backend-node/src/util/migrator/migrator')
+migrator.migrate(argv.path, argv.type)
